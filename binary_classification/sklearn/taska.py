705e71112bc0d968a192d2497d99cb833e0d42b9
meosn <msoloveial@gmail.com>
2025-06-25 18:35:17 +0300

    output

--- binary_classification/sklearn/taska.py
+++ binary_classification/sklearn/taska.py
@@ -9,21 +9,24 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.ensemble import HistGradientBoostingClassifier
+print("\033c")
 
 dataframe = pd.read_csv('binary_classification/sklearn/train.csv') 
 test = pd.read_csv("binary_classification/sklearn/test.csv")
+test_ids = []
 
 def convert(dataframe):
     dataframe["TotalCharges"] = pd.to_numeric(dataframe["TotalCharges"],errors="coerce")
-    dataframe = dataframe.drop(columns=["id"],errors='ignore')
     
-    columns = ["Churn","Partner","Dependents","PhoneService","MultipleLines","OnlineSecurity","OnlineBackup","DeviceProtection","TechSupport","StreamingTV","StreamingMovies","PaperlessBilling"]
+    
+    columns = ["Partner","Dependents","PhoneService","MultipleLines","OnlineSecurity","OnlineBackup","DeviceProtection","TechSupport","StreamingTV","StreamingMovies","PaperlessBilling"]
+    if "Churn" in dataframe.columns:
+        columns.append("Churn")
+        
     dataframe[columns] = dataframe[columns].replace({"Yes":1,"No":0,"No internet service":2,"No phone service":2})
     dataframe["TotalCharges"] = pd.to_numeric(dataframe["TotalCharges"], errors='coerce').fillna(0)
     dataframe["AvgMonthlyCharges"] = dataframe["TotalCharges"]/(dataframe["tenure"]+1)
     dataframe["IsSenior"] = dataframe["SeniorCitizen"]*(1-dataframe["Partner"])
-    # dataframe["AvgChargePerMonth"] = dataframe["TotalCharges"]/(dataframe["tenure"]+1)
-    # dataframe["NewHighPayer"] = ((dataframe["tenure"] < 6) & (dataframe["MonthlyCharges"] > dataframe["MonthlyCharges"].median())).astype(int)
     dataframe["NumServices"] = dataframe[[
     "OnlineSecurity", "OnlineBackup", "DeviceProtection",
     "TechSupport", "StreamingTV", "StreamingMovies"
@@ -31,23 +34,17 @@ def convert(dataframe):
     dataframe["HasStreaming"] = ((dataframe["StreamingTV"] == 1) | (dataframe["StreamingMovies"] == 1)).astype(int)
    
 
-    
-
-
-    
-
-
-
-    
-    
+   
     dataframe["HasPhoneLines"] = (dataframe["PhoneService"] != 0).astype(int)
     dataframe = pd.get_dummies(dataframe).astype(int)
     dataframe["IsFiberUser"] = dataframe.get("InternetService_Fiber optic", 0)
     dataframe["HasInternetButNoServices"] = ((dataframe["NumServices"] == 0) & (dataframe["InternetService_Fiber optic"] == 1)).astype(int)
-    return dataframe
+    test_ids = dataframe["id"]
+    dataframe = dataframe.drop(columns=["id"],errors='ignore')
+    return dataframe,test_ids
 
-dataframe = convert(dataframe)
-test = convert(dataframe)
+dataframe,dfid = convert(dataframe)
+test,test_ids = convert(test)
 dataframe.to_csv("new_file.csv",index=False)
 dataframe.to_html("preview.html")
 
@@ -57,13 +54,6 @@ goal = dataframe["Churn"]
 
 SignsTrain, SignsTest, GoalTrain, GoalTest = train_test_split(signs,goal,test_size=0.2,random_state=42,stratify=goal)
 
-# model = RandomForestClassifier(
-#     n_estimators=3234, #234 - 777
-#     max_depth=15,
-#     min_samples_split=5,
-#     random_state=20,
-#     min_samples_leaf=6,
-#     class_weight="balanced")
 
 gb = GradientBoostingClassifier(
     n_estimators=150,
@@ -88,13 +78,17 @@ ensemble = VotingClassifier(
     weights=[3,9,2]
 )
 
-# model.fit(SignsTrain,GoalTrain)
 ensemble.fit(SignsTrain,GoalTrain)
 PredGoal = ensemble.predict(SignsTest)
 
+prediction = ensemble.predict(test)
+results = pd.DataFrame({
+    "id": test_ids,
+    "Churn": prediction
+}
+)
+results.sort_values("id")
+results.to_html("binary_classification/sklearn/results.html",index=False)
 
-print("Classification report:","\n", classification_report(GoalTest,PredGoal))
-print("confusion matrix:","\n",confusion_matrix(GoalTest,PredGoal))
-
-f1 = f1_score(GoalTest,PredGoal,average="weighted")
-print("weighted F1-score:", round(f1,4))
+# f1 = f1_score(GoalTest,PredGoal,average="weighted")
+# print("weighted F1-score:", round(f1,4))
